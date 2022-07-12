ccb77479403621eba625111d736c444171cec93e
Alberto Corona <muqui@hotmail.com>
2022-07-11 22:22:35 -0500

    the last commit

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -10,6 +10,21 @@ import 'package:flutter/material.dart';
 
 class _CounterscreenState extends State<Counterscreen> {
   int counter = 15; 
+  void increase(){
+    counter++;
+    setState(() {  });
+  }
+
+   void decrease(){
+    counter--;
+    setState(() {  });
+  }
+
+   void reset(){
+    counter = 0;
+    setState(() {  });
+  }
+
   @override
   Widget build(BuildContext context) {
     TextStyle fontSize30 = const TextStyle(fontSize: 30);
@@ -28,42 +43,53 @@ class _CounterscreenState extends State<Counterscreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_minus_1),
-            onPressed: (){
-              counter--;
-              setState(() {
-                
-              });
-            },
-            
-          ),
-          const SizedBox(width: 20),
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_zero),
-            onPressed: (){
-              counter = 0;
-              setState(() {
-                
-              });
-            },
-          ),
+      floatingActionButton:  CustomFloatingActions(
+        increaseFn: increase, 
+        decreaseFn: decrease, 
+        resetFn: reset,
 
-          const SizedBox(width: 20),
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_plus_1),
-            onPressed: (){
-              counter++;
-              setState(() {
-                
-              });
-            },
-          ),
-        ],
       ),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+final Function increaseFn;
+final Function decreaseFn;
+final Function resetFn;
+  const CustomFloatingActions({
+    Key? key, 
+    required this.increaseFn, 
+    required this.decreaseFn,
+    required this.resetFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          child:  const Icon(Icons.exposure_minus_1),
+          onPressed: () => decreaseFn(),
+          //onPressed: ()=> setState(() => counter--),
+          
+        ),
+         const SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_zero),
+          onPressed: () => resetFn(),
+         // onPressed:  ()=> setState(() => counter = 0),
+        ),
+
+         const SizedBox(width: 20),
+        FloatingActionButton(
+          child:  const Icon(Icons.exposure_plus_1),
+          onPressed: () => increaseFn(),
+         // onPressed:  ()=> setState(() => counter++),
+        ),
+      ],
     );
   }
 }
